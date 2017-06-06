c837175de5ea248dbd9f3c33a3364b505fb5a8d2
Eric Estes <eric_estes@aspensquare.com>
2017-06-06 01:39:24 -0400

    WIP

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
+var hbs = require('hbs');
+var routes = require('./routes/index');
 var users = require('./routes/users');
+var compression = require('compression');
+var sassMiddleware = require('node-sass-middleware');
 
 var app = express();
 
@@ -14,33 +16,82 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+// app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(sassMiddleware({
+    /* Options */
+    src: __dirname,
+    dest: path.join(__dirname, 'public'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix:  '/prefix'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use( '/bower_components', express.static( __dirname + '/bower_components' ) );
+app.use( compression() );
 
-app.use('/', index);
+app.use( function ( req, res, next ) {
+    res.header( "Access-Control-Allow-Origin", "*" );
+    res.header( "Access-Control-Allow-Methods", "GET, PUT, POST, DELETE" );
+    res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization" );
+    next();
+} );
+
+//HELPER BLOCKS
+var blocks = {};
+
+hbs.registerHelper( 'extend', function ( name, context ) {
+    var block = blocks[name];
+    if ( !block ) {
+        block = blocks[name] = [];
+    }
+
+    block.push( context.fn( this ) );
+} );
+
+hbs.registerHelper( 'block', function ( name ) {
+    var val = (blocks[name] || []).join( '\n' );
+    blocks[name] = [];
+    return val;
+} );
+
+app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+/// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handler
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+var appEnvironmentConfigs = {
+    getIndexConfig: function ( env ) {
+        var config = {};
+        config.emberName = 'changeset-scope';
+        if ( env === 'development' ) {
+            config.emberBaseUrl = "http://mithrilhall.azurewebsites.net/";
+            config.emberConfig = "%7B%22modulePrefix%22%3A%22mithrilhall%22%2C%22environment%22%3A%22production%22%2C%22rootURL%22%3A%22/%22%2C%22locationType%22%3A%22auto%22%2C%22EmberENV%22%3A%7B%22FEATURES%22%3A%7B%7D%2C%22EXTEND_PROTOTYPES%22%3A%7B%22Date%22%3Afalse%7D%7D%2C%22APP%22%3A%7B%22name%22%3A%22mithrilhall%22%2C%22version%22%3A%220.0.0+62965fd0%22%7D%2C%22exportApplicationGlobal%22%3Afalse%7D"
+        } else {
+            config.emberBaseUrl = "http://mithrilhall.azurewebsites.net/";
+            config.emberConfig = "%7B%22modulePrefix%22%3A%22mithrilhall%22%2C%22environment%22%3A%22production%22%2C%22rootURL%22%3A%22/%22%2C%22locationType%22%3A%22auto%22%2C%22EmberENV%22%3A%7B%22FEATURES%22%3A%7B%7D%2C%22EXTEND_PROTOTYPES%22%3A%7B%22Date%22%3Afalse%7D%7D%2C%22APP%22%3A%7B%22name%22%3A%22mithrilhall%22%2C%22version%22%3A%220.0.0+62965fd0%22%7D%2C%22exportApplicationGlobal%22%3Afalse%7D";
+        }
+        return config;
+    },
+}
+
+
+router.get( '/', function ( req, res ) {
+    res.render( 'index', {
+        config: appEnvironmentConfigs.getIndexConfig( req.app.get( 'env' ) ),
+        user: req.user
+    } );
+} );
 
 module.exports = router;
